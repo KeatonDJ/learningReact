8ce8bba981bdf37489224dbe079a228880b007a5
keatondj <keatondj@iastate.edu>
2018-11-22 21:19:28 -0600

    Updates

---
+++ precache-manifest.9af63ad0da63250d6a3dbf439a6b9c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "04cefbee7a1753cf8fe0",
+    "url": "/react/static/js/main.04cefbee.chunk.js"
+  },
+  {
+    "revision": "ddd5b2f1f2a0fe434094",
+    "url": "/react/static/js/1.ddd5b2f1.chunk.js"
+  },
+  {
+    "revision": "04cefbee7a1753cf8fe0",
+    "url": "/react/static/css/main.385c6bcc.chunk.css"
+  },
+  {
+    "revision": "ddd5b2f1f2a0fe434094",
+    "url": "/react/static/css/1.a6ddb7f8.chunk.css"
+  },
+  {
+    "revision": "2032412794dbb1f806d9f7afb345d2a2",
+    "url": "/react/index.html"
+  }
+];
